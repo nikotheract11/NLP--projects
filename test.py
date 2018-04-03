96f7bd2caf4e429e341da2598ff7b848fb032717
JohnKatsa <johnkatsa66@gmail.com>
2018-04-03 17:13:06 +0300

    save to png file

--- test.py
+++ test.py
@@ -5,30 +5,29 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn import preprocessing
 import pandas as pd
-
+import matplotlib.pyplot as plt
 
 from os import path
 from wordcloud import WordCloud
 
-train_data = pd.read_csv('/home/nikos/Downloads/datasets/train_set.csv', sep="\t")
-train_data = train_data[0:25]
+currdir = path.dirname(__file__)
+
+train_data = pd.read_csv('./datasets/train_set.csv', sep="\t")
 
 categories = set(train_data["Category"])
 for cat in categories:
-    text=train_data[train_data["Category"]==cat]
-    text=text["Content"]
-    text=text.to_string()
-    wordcloud = WordCloud(stopwords=ENGLISH_STOP_WORDS).generate(text)
+	text=train_data[train_data["Category"]==cat]
+	text=text["Content"]
+	text=text.to_string()
+
+	wc = WordCloud(stopwords=ENGLISH_STOP_WORDS)
+	wdcd = wc.generate(text)
+
+	wc.to_file(path.join(currdir, cat + ".png"))
 
     # Display the generated image:
     # the matplotlib way:
-    import matplotlib.pyplot as plt
-    plt.imshow(wordcloud, interpolation='bilinear')
-    plt.axis("off")
 
-    # lower max_font_size
-    wordcloud = WordCloud(max_font_size=40).generate(text)
-    plt.figure()
-    plt.imshow(wordcloud, interpolation="bilinear")
-    plt.axis("off")
-    plt.show()
+	plt.imshow(wdcd, interpolation='bilinear')
+	plt.axis("off")
+	plt.show()
