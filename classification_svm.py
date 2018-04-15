10da18bab6ce1a2e750235c88230e83c2310f37f
JohnKatsa <johnkatsa66@gmail.com>
2018-04-15 15:49:11 +0300

    ..

--- classification_svm.py
+++ classification_svm.py
@@ -15,19 +15,27 @@ from sklearn import svm
 currdir = path.dirname(__file__)
 
 train_data = pd.read_csv('./datasets/train_set.csv', sep="\t")	# './datasets/train_set.csv'
-train_data = train_data[0:25]
+#train_data = train_data[0:25]
 
 le = preprocessing.LabelEncoder()
-le.fit(train_data["Content"])			# "Category"
+le.fit(train_data["Category"])			# "Category"
 
-y = le.transform(train_data["Content"])	# "Category"
+y = le.transform(train_data["Category"])	# "Category"
 set(y)
 print y
 
 count_vectorizer = CountVectorizer(stop_words=ENGLISH_STOP_WORDS)
-X = count_vectorizer.fit_transform(train_data["Content"])
+X = count_vectorizer.fit_transform(train_data["Category"])
 
 print X		# vector of all columns in identifiers
 
 clf = svm.SVC()
 clf.fit(X,y)
+
+y_pred = clf.predict(X)
+y_pred
+
+predicted_categories = le.inverse_transform(y_pred)
+print predicted_categories
+
+print classification_report(y, y_pred, target_names=list(le.classes_))
