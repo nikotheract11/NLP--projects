98fe60e5d6de97a44dd00190ff36caa1692a4ede
Nikos <nikotheract11@gmail.com>
2018-04-15 22:58:37 +0300

    wordcloud fixes

--- images.py
+++ images.py
@@ -4,16 +4,27 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn import preprocessing, svm
+
+import numpy as np
+from PIL import Image
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from os import path
-from wordcloud import WordCloud
+from wordcloud import WordCloud,ImageColorGenerator
 
 from sklearn import svm
 
 currdir = path.dirname(__file__)
 
-train_data = pd.read_csv('./NLP--projects/datasets/train_set.csv', sep="\t")
+alice_mask = np.array(Image.open(path.join(currdir, "drop.png")))
+
+
+train_data = pd.read_csv('./datasets/train_set.csv', sep="\t")
+sw = set(ENGLISH_STOP_WORDS)
+sw.add("say")
+sw.add("said")
+sw.add("new")
 
 categories = set(train_data["Category"])
 for cat in categories:
@@ -21,9 +32,7 @@ for cat in categories:
 	text=text["Content"]
 	text=text.to_string()
 
-	wc = WordCloud(stopwords=ENGLISH_STOP_WORDS)
+	wc = WordCloud(background_color="white", mask=alice_mask,
+	stopwords=sw,random_state=18998)
 	wdcd = wc.generate(text)
-
 	wc.to_file(path.join(currdir, cat + ".png"))
-
-
