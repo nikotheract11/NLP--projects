2b2804b92935f2324f0ce2593f68767e5c6e3651
JohnKatsa <johnkatsa66@gmail.com>
2018-04-24 17:59:55 +0300

    knn ready

--- knn.py
+++ knn.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from __future__ import division
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import RandomForestClassifier
@@ -24,56 +25,69 @@ def loadData_Preprocess():
     currdir = path.dirname(__file__)
 
     train_data = pd.read_csv('./datasets/train_set.csv', sep="\t")	# './datasets/train_set.csv'
-    train_data = train_data[0:50]
+    train_data = train_data[0:25]
+
+    X_train, X_test, y_train, y_test = train_test_split(train_data, train_data["Category"], test_size=0.33, random_state=42)
+    print X_test, y_test
 
     le = preprocessing.LabelEncoder()
-    le.fit(train_data["Category"])
-    y = le.transform(train_data["Category"])
-    #set(y)
+    le.fit(y_train)
+    y = le.transform(y_train)
+
+    le2 = preprocessing.LabelEncoder()
+    le2.fit(y_test)
+    y2 = le2.transform(y_test)
 
     count_vectorizer = CountVectorizer(stop_words=ENGLISH_STOP_WORDS)
-    X = count_vectorizer.fit_transform(train_data["Content"])
-    #X = list(X)
+    X = count_vectorizer.fit_transform(X_train["Content"])
+    X2 = count_vectorizer.fit_transform(X_test["Content"])
+
     X = X.toarray()
-    return X,y,train_data,le
+    X2 = X2.toarray()
+
+    return X,X2,y,y2,train_data,le
 
 # Calculate euclidean distance
 def euclideanDistance(instance1, instance2, j):    # testInstance1 is number, instance2 is not
     distance = 0
-    for i in range(j):  # j for components
-        distance += pow(instance1-instance2[i],2)
+    for i in range(0,1):  # j for components
+        distance += pow(instance2[i]-instance1[i],2)
     result = math.sqrt(distance)
     return result
 
 def getNeighbours(X, testInstance, k):
 	distances = []; j = len(X[0])
-	for x in X:
-		dist = euclideanDistance(testInstance, x, j)  # j = length of row
-		distances.append([x, dist, testInstance])
+	for i in range(0,len(X)):
+		dist = euclideanDistance(testInstance, X[i], j)  # j = length of row
+		distances.append([X[i], dist, i])
 
 	distances.sort(key = itemgetter(1))
 	neighbours = []
 	for i in range(k):
 		neighbours.append(distances[i])
         #print "i = ", distances[i]
-	neighbours = neighbours[0:k];  return neighbours
+	return neighbours
 
 def majorityvote(neighbours, X, le, y, k):
     categories = []
     s = []
     for i in range(k):
-        categories.append(neighbours[0][i][2])
+        categories.append(y[neighbours[0][i][2]])
     #print "categories = ", categories
-    s = le.inverse_transform(categories)    # contains all categories
+    #s = le.inverse_transform(categories)    # contains all categories
     #print "s = ", s
-    cnt = Counter(s)
+    cnt = Counter(categories)
+    print cnt
     return cnt.most_common(1)[0][0]
 
 # nearest_neighbour --> main
 def nearest_neighbour():
-    X ,y, train_data, le = loadData_Preprocess()
+    X, X2, y, y2, train_data, le = loadData_Preprocess()
     print "X = ", X
+    print "X2 = ", X2
     print "y = ", y
+    print "len(y2) = ", len(y2)
+    print "len(X2) = ", len(X2)
 
     neighbours = []
     k = 2
@@ -81,16 +95,17 @@ def nearest_neighbour():
     success = 0;
     failure = 0;
 
-    for yi in y:
+    for i in range(0,len(X2)):    # for every testInstance
         neighbours = []
-        neighbours.append(getNeighbours(X,yi,k))    # returns k list items with 2 components
+        neighbours.append(getNeighbours(X,X2[i],k))    # returns k list items with 2 components
         ela = majorityvote(neighbours, X, le, y, k) # the answer
-        if ela == yi:
+        print ela, " ", y2[i]
+        if ela == y2[i]:
             success += 1
         else:
             failure += 1
-        #print "answer is : ", ela
 
-    print "success rate = ", float(success/len(y))
+    var = success/len(y2)
+    print "success rate = ", var
 
 nearest_neighbour()
