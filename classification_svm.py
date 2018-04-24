e6c47c3ddcd915f54f5bc1dba08f44baa981cac9
Nikos <nikotheract11@gmail.com>
2018-04-24 09:46:40 +0300

    LSI components and plot

--- classification_svm.py
+++ classification_svm.py
@@ -9,7 +9,7 @@ from sklearn import preprocessing, svm
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.model_selection import GridSearchCV,train_test_split
+from sklearn.model_selection import GridSearchCV,train_test_split,cross_val_score
 
 from os import path
 
@@ -20,7 +20,7 @@ from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_sc
 currdir = path.dirname(__file__)
 
 train_data = pd.read_csv('./datasets/train_set.csv', sep="\t")	# './datasets/train_set.csv'
-train_data = train_data[0:201]
+train_data = train_data[0:200]
 
 train,test = train_test_split(train_data,test_size=0.33,random_state=42)
 '''print train_data["Category"][0]
@@ -36,25 +36,39 @@ print y
 #clf = svm.SVC()
 
 count_vectorizer = CountVectorizer(stop_words=ENGLISH_STOP_WORDS)
-X = count_vectorizer.fit_transform(train["Content"]+4*(" "+train["Title"]))
+#X = count_vectorizer.fit_transform(train["Content"]+4*(" "+train["Title"]))
 X_test = count_vectorizer.fit_transform(test["Content"]+4*(" "+test["Title"]))
 
 # LSI
 ac = []
 
-#for i in range(1,70):
-#svd = TruncatedSVD(n_components=5,random_state=42)
-#svd.fit(X)
-#svd.fit_transform(X)
-#svd.fit_transform(X_test)
-clf = svm.SVC()
-clf.fit(X,y)
-y_pred = clf.predict(X_test)
-ac.append(accuracy_score(y,y_pred))
+'''for i in range(1,70):
+	X = count_vectorizer.fit_transform(train["Content"]+4*(" "+train["Title"])
+	svd = TruncatedSVD(n_components=i,n_iter=100,random_state=42)
+	#svd.fit(X)
+	X=svd.fit_transform(X)
+#	svd.fit_transform(X_test)
+	clf = svm.SVC()
+	clf.fit(X,y)
 
-'''ss=range(1,70)
-plt.plot(ss,ac)
-plt.show()'''
+#	y_pred = clf.predict(X)
+	scores=cross_val_score(clf,X,y,cv=10,scoring='accuracy')
+	ac.append(scores.mean())'''
+
+d=[]
+from sklearn.decomposition import TruncatedSVD
+for i in range(1,100):
+    X=count_vectorizer.fit_transform(train['Content']+4*(" "+train["Title"]))
+    lsa=TruncatedSVD(n_components=i)
+    X=lsa.fit_transform(X)
+    clf = svm.SVC()
+    clf.fit(X,y)
+    scores=cross_val_score(clf,X,y,cv=10,scoring='accuracy')
+    d.append(scores.mean())
+
+ss=range(1,30)
+plt.plot(ss,d)
+plt.show()
 
 print X		# vector of all columns in identifiers
 print "======================================================="
